f07f3297d6d8f581b65fb86399d8a93477f8f9ea
Lucas Gabriel <90lucasgabriel@gmail.com>
2021-04-13 20:35:42 -0300

    09 - New: Contact page components;

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:contacts/ui/home_page.dart';
+import 'package:contacts/ui/contact_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
 import 'package:contacts/helpers/contact_helper.dart';
 
 class ContactPage extends StatefulWidget {
@@ -12,6 +14,11 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
   Contact _contact;
+  bool _hasChanges = false;
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
 
   @override
   void initState() {
@@ -23,6 +30,9 @@ class _ContactPageState extends State<ContactPage> {
     }
 
     _contact = Contact.toObject(widget.selectedContact.toMap());
+    _nameController.text = _contact.name;
+    _emailController.text = _contact.email;
+    _phoneController.text = _contact.phone;
   }
 
   @override
@@ -37,6 +47,67 @@ class _ContactPageState extends State<ContactPage> {
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
       ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 140,
+                width: 140,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                  image: _contact.image != null
+                      ? DecorationImage(image: FileImage(File(_contact.image)))
+                      : null,
+                ),
+                child: Center(
+                  child: _contact.name != null && _contact.name.isNotEmpty
+                      ? Text(
+                          _contact.name.toUpperCase().substring(0, 1),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 80,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        )
+                      : Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 100,
+                        ),
+                ),
+              ),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Nome'),
+                onChanged: (value) {
+                  _hasChanges = true;
+                  setState(() {
+                    _contact.name = value;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  _hasChanges = true;
+                  _contact.email = value;
+                },
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: 'Phone'),
+                keyboardType: TextInputType.phone,
+                onChanged: (value) {
+                  _hasChanges = true;
+                  _contact.phone = value;
+                },
+              )
+            ],
+          )),
     );
   }
 }
